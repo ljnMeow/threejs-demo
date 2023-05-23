2010642d2cba5c2acc6d62959cf8bc2e2622fe44
linMeow <linjinnan1998@sina.com>
2023-05-23 16:38:51 +0800

    building

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,30 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import compression from "vite-plugin-compression";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: './'
-})
+  plugins: [
+    vue(),
+    compression({
+      algorithm: "gzip",
+      ext: ".gz",
+    }),
+  ],
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
+  base: "./",
+});
